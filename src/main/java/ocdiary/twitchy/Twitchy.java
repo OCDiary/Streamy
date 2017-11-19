bc25ed41c99ef37aea237d634a4a3a57d1a4278a
OCDiary <obsessivecompulsivediary@gmail.com>
2017-11-19 00:49:54 +0000

    version update

--- src/main/java/ocdiary/twitchy/Twitchy.java
+++ src/main/java/ocdiary/twitchy/Twitchy.java
@@ -15,7 +15,7 @@ public class Twitchy {
 
     public static final String MODID = "twitchy";
     public static final String NAME = "Twitchy";
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.12.2_2";
     public static final String AVERSION = "[1.12, 1.12.2]";
 
     public static final String CPROX = "ocdiary.twitchy.proxies.ClientProxy";
